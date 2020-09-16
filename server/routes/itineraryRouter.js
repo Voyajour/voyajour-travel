d90b418546c7c4f1c961fe7c48ec3250ce0513d9
Jesus <jmodestov@gmail.com>
2020-09-16 16:46:21 -0700

    Started working on update activity functionality

--- server/routes/itineraryRouter.js
+++ server/routes/itineraryRouter.js
@@ -11,4 +11,22 @@ router.post('/',
     res.sendStatus(200);
   });
 
+//router update update activity
+//activities table columns: _id, location_id, user_id, link, notes, address, completed, description
+//id is in params
+router.put('/updateActivity',itineraryController.updateActivity, (req, res) => {
+  //if res.locals.sucess is true then send the updatedActivity object back tot he client
+  if(res.locals.sucess){
+    res.status(200).json(res.locals.updatedActivity);
+  } else {
+    res.status(200).send('Sorry. Activity could not be updated at this time.');
+  }
+})
+
+//router update location
+router.put('/updateLocation', itineraryController.updateActivity,(req, res) => {
+  res.sendStatus(200);
+
+})
+
 module.exports = router;
