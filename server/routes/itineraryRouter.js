22c71157fba2d9d25292ca87c6b8111fa51fe896
AlexanderLanderos <Alexander.Landeros1@gmail.com>
2020-09-16 16:40:05 -0700

    server router newLocation and newLocation controller

--- server/routes/itineraryRouter.js
+++ server/routes/itineraryRouter.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-const itineraryController = require('../controllers/itineraryController.js')
+const itineraryController = require('../controllers/itineraryController.js');
 
-router.post('/',
-  itineraryController.newPlan,
-  (req, res) => {
-    console.log('posted into db @ iteneraryController');
-    res.sendStatus(200);
-  });
+// creating a new location in the locations table.
+router.post('/newLocation', itineraryController.newLocation, (req, res) => {
+  if (res.locals.success) {
+    console.log('posted into db @ itineraryController');
+    res.status(200).json(res.locals.newLocation);
+  } else {
+    res.status(200).send('My apologies, could not update database');
+  }
+});
+
+// router.post with that of /newActivity
+// router.post('/newActivity', itineraryController., (req, res) => {
+//   // info needed for new activity _id, location_id, user_id, link, notes, address, completed, description
+//   console.log('new activity created in DB @ itineraryController');
+//   res.status(200).json(res.locals);
+// });
 
 module.exports = router;
